0a6c4b705aa7242f600968aff4febd5b4dbbfb9e
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-11-14 23:59:55 +0500

    Аетбаева Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,19 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int prevNum = System.in.read();
+        int nextNum;
+        while (prevNum != -1){
+            nextNum = System.in.read();
+            if (prevNum != 13 || nextNum != 10){
+                System.out.write(prevNum);
+            }
+            prevNum = nextNum;
+        }
+        System.out.flush();
     }
 }
