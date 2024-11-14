9c4fa5ead691f897c95c401608f5a4139b3872c1
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-11-14 23:59:32 +0500

    Аетбаева Задание 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,15 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int cSum = 0;
+        int value;
+        while ((value = inputStream.read()) != -1) {
+            cSum = Integer.rotateLeft(cSum, 1) ^ value;
+        }
+        return cSum;
     }
 }
